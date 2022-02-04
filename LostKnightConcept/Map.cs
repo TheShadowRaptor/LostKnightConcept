e9743e9673adccfb9c9fe0ce56387e26a73fdaf4
TheShadowRaptor <90637945+TheShadowRaptor@users.noreply.github.com>
2022-02-04 14:57:21 -0400

    Initial commit

---
+++ LostKnightConcept/Map.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LostKnightConcept
+{
+    class Map
+    {
+        static private char[,] map;
+
+        public Map()
+        {
+            map = new char[,] 
+            {
+                {'*','*','*','*','*','*'},
+                {'*','*','*','*','*','*'},
+                {'*','*','*','*','*','*'},
+                {'*','*','*','*','*','*'},
+                {'*','*','*','*','*','*'},
+                {'*','*','*','*','*','*'},
+            };
+        }
+
+        public void DisplayMap()
+        {
+            for (int x = 0; x < map.GetLength(0); x++)
+            {
+                for (int y = 0; y < map.GetLength(1); y++)
+                {
+                    Console.Write(map[x, y]);
+                }
+            }
+        }
+    }
+}
---
+++ LostKnightConcept/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LostKnightConcept
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Map map = new Map();
+
+            map.DisplayMap();
+            Console.ReadKey(true);
+        }
+    }
+}
