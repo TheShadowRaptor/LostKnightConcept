c581ac201d9946067397db5b7f6228dfc88ffc65
TheShadowRaptor <90637945+TheShadowRaptor@users.noreply.github.com>
2022-02-05 18:13:08 -0400

    [ADDED] Map & map colour

--- LostKnightConcept/Map.cs
+++ LostKnightConcept/Map.cs
@@ -14,23 +14,63 @@ namespace LostKnightConcept
         {
             map = new char[,] 
             {
-                {'*','*','*','*','*','*'},
-                {'*','*','*','*','*','*'},
-                {'*','*','*','*','*','*'},
-                {'*','*','*','*','*','*'},
-                {'*','*','*','*','*','*'},
-                {'*','*','*','*','*','*'},
+                {'*','*','*','*','*','*','*','*','*','*','*','*','*','*','*','*','*','*','*','*','*','*','*'},
+                {'*','*','*','*','*','*','*','*','*','*','*','*','*','*','*','*','*','*','*','*','*','*','*'},
+                {'*','*','*','*','*','*','*','*','*','*','*','*','*','*','*','*','*','*','*','*','*','*','*'},
+                
             };
         }
 
         public void DisplayMap()
         {
+            //------------------Top Border--------------------
+            Console.Write("╔");
+            for (int i = 0; i < map.GetLength(1); i++)
+            {
+                Console.Write("═");
+            }
+            Console.Write("╗");
+            Console.WriteLine("");
+
+            //---------------------Map------------------------           
+
+            //
             for (int x = 0; x < map.GetLength(0); x++)
             {
+                Console.Write("║");
+
                 for (int y = 0; y < map.GetLength(1); y++)
                 {
+                    //Colour the Map
+                    ColourMap(x, y);
+
+                    //Draws map
                     Console.Write(map[x, y]);
+
+                    //Resets colour
+                    Console.ResetColor();
                 }
+                Console.WriteLine("║");
+            }           
+
+
+            //------------------Bottom Border--------------------
+            Console.Write("╚");
+            for (int i = 0; i < map.GetLength(1); i++)
+            {
+                Console.Write("═");
+            }
+            Console.Write("╝");
+            Console.WriteLine("");
+        }
+
+        public void ColourMap(int x, int y)
+        {
+
+            if (map[x, y] == '*')
+            {
+                Console.ForegroundColor = ConsoleColor.Green;
+                Console.BackgroundColor = ConsoleColor.Green;
             }
         }
     }
